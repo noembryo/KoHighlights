2481226c354097ebdd87200229d12ac0819d2fca
noembryo <noembryo@gmail.com>
2019-04-01 02:48:29 +0300

    1.0.0.0

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name="KoHighlights", version="1.0.0.0", author="noEmbryo",
-      packages=["future", "mechanize", "beautifulsoup4", "pyside"],
+      packages=[""],
       url="https://github.com/noembryo/KoHighlights", license="MIT",
       author_email="noembryo@gmail.com",
       description=("A utility for viewing KoReader's highlights and/or "
